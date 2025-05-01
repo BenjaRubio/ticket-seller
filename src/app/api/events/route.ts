2ac5ae3713fe10d2b3bf537769d0f926f2a02df9
benjarubio <barubio@uc.cl>
2025-05-01 04:50:58 -0400

    fix: added force dynamic on evets route

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from 'next/server';
 import { createEvent, getEvents } from './events.controller';
 
+export const dynamic = 'force-dynamic';
+
 export async function GET() {
   try {
     const events = await getEvents();
