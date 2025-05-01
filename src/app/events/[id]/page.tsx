ad56b3591ae1f1e2235f80de044316c697a00158
benjarubio <barubio@uc.cl>
2025-05-01 03:43:17 -0400

    fix: calling dynamic host on events id page

--- src/app/events/[id]/page.tsx
+++ src/app/events/[id]/page.tsx
@@ -1,27 +1,25 @@
 import { EventDetailContainer } from "@/components/events/eventDetailContainer";
-import { getEventById } from "@/app/api/events/events.controller";
 import { notFound } from 'next/navigation';
 import { IEvent } from "@/app/api/events/events.interface";
+import { headers } from 'next/headers';
 
 export default async function EventPage({ params }: { params: { id: string } }) {
+  const headersList = headers();
+  const host = headersList.get('host') || 'localhost:3000';
+  const protocol = process.env.NODE_ENV === 'development' ? 'http' : 'https';
+  
   const eventId = params.id;
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/events/${eventId}`, {
+  const response = await fetch(`${protocol}://${host}/api/events/${eventId}`, {
     cache: 'no-store'
-  }).then((res) => res.json());
-  const event = response.ok ? response.data as IEvent : null;
-  // const event = (await getEventById(eventId)) as IEvent | null;
+  });
+  const data = await response.json();
+  const event = data.ok ? data.data as IEvent : null;
+  
+  if (!event) return notFound();
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-foreground">
-      {event ?
-        <EventDetailContainer event={event} />
-        :
-        <div className="flex flex-col items-center justify-center h-screen bg-foreground">
-          <h1 className="text-4xl font-bold text-primary-foreground">
-            No se pudo cargar el evento
-          </h1>
-        </div>
-      }
+      <EventDetailContainer event={event} />
     </div>
   );
 }
